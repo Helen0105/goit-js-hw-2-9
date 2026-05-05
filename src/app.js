81d906e4ad4d827c1547ab710838f6a88a8ac93a
Helen0105 <marinenkoolena21@gmail.com>
2026-05-05 21:32:19 +0300

    1

--- src/app.js
+++ src/app.js
@@ -28,47 +28,48 @@
 
 
 
-const nameInput = document.querySelector('.name-input');
-const phoneInput = document.querySelector('.phone-input');
+const name = document.querySelector('.name-input');
+const phone = document.querySelector('.phone-input');
 const button = document.querySelector('.btn');
-const contactList = document.querySelector('.contact-list');
+const contact = document.querySelector('.contact-list');
 
-const renderContact = (name, phone) => {
+function renderContact(userName, userPhone) {
     const li = document.createElement('li');
     li.innerHTML = `
-        <span><strong>${name}</strong>: ${phone}</span>
+        <span><strong>${userName}</strong>: ${userPhone}</span>
         <button class="delete-btn">Видалити</button>
     `;
 
-    li.querySelector('.delete-btn').onclick = () => {
+    li.querySelector('.delete-btn').onclick = function() {
         li.remove();
-        deleteContact(name);
-    };
-
-    contactList.appendChild(li);
-};
-
-
-const savedContacts = JSON.parse(localStorage.getItem('contacts')) || {};
-Object.keys(savedContacts).forEach(name => {
-    renderContact(name, savedContacts[name]);
-});
-
-button.onclick = () => {
-    const name = nameInput.value.trim();
-    const phone = phoneInput.value.trim();
-
-    if (name && phone) {
-
-        renderContact(name, phone);
 
 
         const contacts = JSON.parse(localStorage.getItem('contacts')) || {};
-        contacts[name] = phone;
+        delete contacts[userName];
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    };
+
+    contact.appendChild(li); 
+}
+
+const savedContacts = JSON.parse(localStorage.getItem('contacts')) || {};
+for (const n in savedContacts) {
+    renderContact(n, savedContacts[n]);
+}
+
+button.onclick = function() {
+    const nameVal = name.value;
+    const phoneVal = phone.value;
+
+    if (nameVal !== '' && phoneVal !== '') {
+        renderContact(nameVal, phoneVal);
+
+        const contacts = JSON.parse(localStorage.getItem('contacts')) || {};
+        contacts[nameVal] = phoneVal;
         localStorage.setItem('contacts', JSON.stringify(contacts));
 
-        nameInput.value = '';
-        phoneInput.value = '';
+        name.value = '';
+        phone.value = '';
     }
 };
 
